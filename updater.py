22e2071c8de51717fe7ceeb48937d47d26d5056d
G-enterpriseGroup <143067071+G-enterpriseGroup@users.noreply.github.com>
2025-09-18 19:46:15 -0400

    Update and rename update_sheets2.py to updater.py

--- updater.py
+++ updater.py
@@ -6,8 +6,8 @@ from gspread_dataframe import set_with_dataframe
 from google.oauth2.service_account import Credentials
 
 # --------- Env / defaults ---------
-SHEET_URL      = os.environ.get("SHEET_URL", "").strip()
-WORKSHEET_NAME = os.environ.get("WORKSHEET_NAME", "Sheet1").strip()
+SHEET_URL      = os.environ.get("https://docs.google.com/spreadsheets/d/1Km_RB87aNI0TOD16Q8PFiijaa4wDxLx_He2P59g2Q4w/edit?usp=sharing", "").strip()
+WORKSHEET_NAME = os.environ.get("Report Pro", "Sheet1").strip()
 
 CELL_TICKER = "A2"
 CELL_OCC    = "A3"
